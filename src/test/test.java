0da2b7575ba48d2d84d869769b71a514b8bab064
esrrhs <esrrhs@localhost>
2016-07-18 17:57:49 +0800

    update

--- src/test/test.java
+++ src/test/test.java
@@ -7,7 +7,17 @@ public class test
 	public static void main(String[] args)
 	{
 		fake f = fk.newfake(null);
-		fk.set_callback(f, new callbackimpl());
+		fk.set_callback(f, new callback() {
+			@Override
+			public void on_error(fake f, String file, int lineno, String func, String str)
+			{
+				System.out.printf("fake error in file(%s:%d) func(%s) : ", file, lineno, func);
+				System.out.printf("%s\n", str);
+				// TODO
+				//System.out.printf("call stack :\n%s\n", fk.getcurcallstack(f));
+				//System.out.printf("cur routine :\n%s\n", fk.getcurroutine(f));
+			}
+		});
 		
 		boolean ret = fk.parse(f, "./src/test/test.fk");
 		System.out.println("hehe");
