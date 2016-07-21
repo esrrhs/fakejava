419d3b77c38fb52d2e0dab199f9bff8119dbf8a9
esrrhs <esrrhs@163.com>
2016-07-21 18:04:08 +0800

    update

---
+++ src/fakescript/compiler.java
@@ -0,0 +1,19 @@
+package fakescript;
+
+class compiler 
+{
+	fake m_f;
+	mycup m_mcp;
+	
+	public compiler(fake f, mycup mcp)
+	{
+		m_f = f;
+		m_mcp = mcp;
+	}
+	
+	public boolean compile()
+	{
+		// TODO
+		return true;
+	}
+}
