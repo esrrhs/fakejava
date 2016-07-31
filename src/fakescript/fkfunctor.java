41618745caffc2ac0e9cda7ca58f4842c7159913
esrrhs <esrrhs@163.com>
2016-07-31 11:07:39 +0800

    update

---
+++ src/fakescript/fkfunctor.java
@@ -0,0 +1,11 @@
+package fakescript;
+
+import java.lang.reflect.Method;
+
+class fkfunctor
+{
+	Class<?> m_c;
+	Method m_m;
+	Class<?>[] m_param;
+	Class<?> m_ret;
+}
--- src/fakescript/funcunion.java
+++ src/fakescript/funcunion.java
@@ -2,10 +2,10 @@ package fakescript;
 
 class funcunion
 {
-	//public fkfunctor m_ff;
-	//public bifunc m_bif;
+	public fkfunctor m_ff;
+	// public bifunc m_bif;
 	public func_binary m_fb;
-	//public boolean m_haveff;
-	//public boolean m_havebif;
+	public boolean m_haveff;
+	// public boolean m_havebif;
 	public boolean m_havefb;
 }
