4a1489a7f9f890ee076bd1de27077c47bb798f92
bjzhaoxin <bjzhaoxin@corp.netease.com>
2017-06-09 14:19:51 +0800

    fix bug

--- src/fakescript/funcunion.java
+++ src/fakescript/funcunion.java
@@ -14,7 +14,7 @@ class funcunion
 		funcunion fc = new funcunion();
 		fc.m_ff = this.m_ff;
 		fc.m_bif = this.m_bif;
-		fc.m_fb = this.m_fb.clonef();
+		fc.m_fb = this.m_fb != null ? this.m_fb.clonef() : null;
 		fc.m_haveff = this.m_haveff;
 		fc.m_havebif = this.m_havebif;
 		fc.m_havefb = this.m_havefb;
