c17d5448ed17098b18a2c69390a1c6583b5c4f1c
bjzhaoxin <bjzhaoxin@corp.netease.com>
2017-11-30 19:29:04 +0800

    add

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 cd jflexbison
 jflex jflex.flex
-mv Yylex.java ../src/com/github/esrrhs/fakescript/Yylex.java -f
+mv Yylex.java ../src/main/java/com/github/esrrhs/fakescript/Yylex.java -f
 
 bison -L JAVA YYParser.y
-mv YYParser.java ../src/com/github/esrrhs/fakescript/YYParser.java -f
+mv YYParser.java ../src/main/java/com/github/esrrhs/fakescript/YYParser.java -f
 
 
